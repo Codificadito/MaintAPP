a5dcb675b0b511547f733c3df0a2d7a4ab78167c
Iara Baya <iarabayav@gmail.com>
2023-11-10 06:35:42 -0300

    feat: reutilizacion de la pantalla comentarios como fragment

---
+++ app/src/main/java/com/example/maintapp/ui/comment/CommentsFragment.kt
@@ -0,0 +1,20 @@
+package com.example.maintapp.ui.comment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.maintapp.R
+
+class CommentsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_comments, container, false)
+    }
+
+}
